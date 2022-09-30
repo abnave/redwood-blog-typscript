6131ecc0d1fd48fbca46f0859f34209a8dc8fa54
ShubhamAbnaveInfogenLabs <shubham.a@infogen-labs.com>
2022-09-30 14:00:14 +0530

    contact creation

--- web/src/pages/ArticlePage/ArticlePage.tsx
+++ web/src/pages/ArticlePage/ArticlePage.tsx
@@ -10,11 +10,6 @@ const ArticlePage = ({ id }: Props) => {
   return (
     <>
       <MetaTags title="Article" description="Article page" />
-
-      <h1>ArticlePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ArticlePage/ArticlePage.tsx</code>
-      </p>
       <ArticleCell id={id} />
     </>
   )
